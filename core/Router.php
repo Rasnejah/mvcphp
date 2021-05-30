966167017c81a219d61eb2271c035ff90186dcf6
RasneJah <renatososmicro@gmail.com>
2021-05-30 11:44:39 -0300

    armazenando dados nos atributos do Router

--- core/Router.php
+++ core/Router.php
@@ -11,10 +11,17 @@ class Router
     {
         $router = $this->url();
         if(file_exists('app/controllers/'. ucfirst($router[0]). '.php')): 
-            echo 'arquivo existe';
-        else: 
-            echo 'aruivo nao existe';
+            $this->controller = $router[0];
+            unset($router[0]);
         endif;
+        $class = "\\app\\controllers\\". ucfirst($this->controller);
+        $object = new $class;
+        if(isset($router[1]) and method_exists($class, $router[1])): 
+            $this->method = $router[1];
+            unset($router[1]);
+        endif;
+        $this->param = $router ? array_values($router) : [];
+        call_user_func_array([$object,$this->method], $this->param);
     }
 
     private function url()

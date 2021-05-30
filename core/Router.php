7bd15da0d896665c913f9587bea636671aee9646
RasneJah <renatososmicro@gmail.com>
2021-05-30 10:43:08 -0300

    configurando Router

--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,17 @@ namespace core;
 
 class Router
 {
+    private $controller = 'Site';
+    private $method = "home";
+    private $param = [];
     public function __construct()
     {
       
+    }
+
+    private function url()
+    {
+        $parse_url = explode("/",filter_input(INPUT_GET, 'router', FILTER_SANITIZE_URL));
+        return $parse_url;
     }
 }

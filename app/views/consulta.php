4baf4cc1599cf886442ce2f5d5848cfd9d458e89
RasneJah <renatososmicro@gmail.com>
2021-06-01 06:26:49 -0300

    ajustando tela de consulta

--- app/views/consulta.php
+++ app/views/consulta.php
@@ -1,13 +1,28 @@
 <div class="row container">
     <div class="col s12">
         <h1 class="light">Pagina Consulta</h1>
-        <h2>
-            <?php foreach($registrationQuery as $register): ?>
-                <p>
-                    nome: <?php echo $register['name'];?><br>
-                    email:<?php echo $register['email'];?>
-                </p>
-            <?php endforeach; ?>
+        
+            <table class="striped">
+                <thead>
+                    <tr>
+                        <th>Nome:</th>
+                        <th>Email</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+            <tbody>
+                <?php foreach($registrationQuery as $register): ?>
+                    <tr>
+                        <td><?php echo $register['name'];?></td>
+                        <td><?php echo $register['email'];?></td>
+                        <td>
+                            <a href="?router=Site/editar/&id=<?php echo $register['id']?>" class="btn-small">Alterar</a>
+                            <a href="?router=Site/deletar/&id=<?php echo $register['id']?>" class="btn-small red">excluir</a>
+                        </td>
+                    </tr>                   
+                <?php endforeach; ?>
+            </tbody>
+            </table>
         </h2>
     </div>
 </div>
